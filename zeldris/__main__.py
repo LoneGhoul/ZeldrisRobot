0d759c887288ca5e53a0b200170beeaae3f943c4
Nine bish <88079604+ghoul-lover@users.noreply.github.com>
2021-12-21 22:40:52 +0530

    Update __main__.py

--- zeldris/__main__.py
+++ zeldris/__main__.py
@@ -96,7 +96,7 @@ Caption = "contact me in pm"
 ZELDRIS_IMG = "https://telegra.ph/file/d4f2e6ed670ab3edd0195.jpg"
 
 PM_START_TEXT = """
-Hᴇʟʟᴏ ᴛʜᴇʀᴇ [ᴍɪᴋᴇʏ](https://telegra.ph/file/b1573113bddc5613b8f29.jpg) ʜᴇʀᴇ 
+Hᴇʟʟᴏ ᴛʜᴇʀᴇ [ᴍɪᴋᴇʏ](https://telegra.ph/file/56039f02035c2b0825fdf.jpg) ʜᴇʀᴇ 
 ━━━━━━━━━━━━━━━━━━━━━━
 ᴀᴛ ʏᴏᴜʀ sᴇʀᴠɪᴄᴇ ғʀᴏᴍ ᴛᴏᴋʏᴏ ʀᴇᴠᴇɴɢᴇʀs
 ━━━━━━━━━━━━━━━━━━━━━━
